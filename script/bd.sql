59ba833b9562dc7fac38168c1fb36dc2a4d342b4
isaacGon <isaac.gz.ov@gmail.com>
2022-11-18 10:24:06 -0600

    Script bd

---
+++ script/bd.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS arkus;
+
+USE arkus;
+
+CREATE TABLE IF NOT EXISTS Usuarios(
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Correo VARCHAR(50),
+    Contrasena VARCHAR(50),
+    Curp VARCHAR(20),
+    Telefono VARCHAR(10)
+);
